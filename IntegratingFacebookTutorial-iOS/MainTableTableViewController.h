bc69ef60b6fb01c0408c273d2ee0dc467456c21f
dvirdi <dvirdi@scu.edu>
2014-05-19 15:09:22 -0700

    firebase changes

---
+++ IntegratingFacebookTutorial-iOS/MainTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableTableViewController.h
+//  speakEasy
+//
+//  Created by Daljeet Virdi on 5/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableTableViewController : UITableViewController
+
+@end
